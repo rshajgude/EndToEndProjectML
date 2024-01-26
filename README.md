b1cbcfbc6d4218eba4174dc3e67cb919f495f0a7
Rajkumar Hajgude <rshajgude@gmail.com>
2024-01-27 01:27:02 +0530

    new proj

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Test line from VS code
 Test line
 
 conda create -p venv python==3.7 -y
+
+Build image name -
+docker build -t image_name:tag 
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Raj"
+DESCRIPTION="Test Test"
+PAKAGES=["housing"]
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    pakages=PAKAGES
+
+)
